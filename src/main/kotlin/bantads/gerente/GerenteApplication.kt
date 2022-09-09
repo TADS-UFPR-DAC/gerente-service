7f3e4199e06c9845f2e350c693afdcb9b2dfac8c
Gabriel Ganssin <gabriel.ganassin@contabilizei.com.br>
2022-09-09 11:37:00 -0300

    queue consumer

--- src/main/kotlin/bantads/gerente/GerenteApplication.kt
+++ src/main/kotlin/bantads/gerente/GerenteApplication.kt
@@ -1,7 +1,9 @@
 package bantads.gerente
 
+import bantads.gerente.queue.Consumer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class GerenteApplication
---
+++ src/main/kotlin/bantads/gerente/queue/Consumer.kt
@@ -0,0 +1,51 @@
+package bantads.gerente.queue
+
+import bantads.gerente.service.GerenteService
+import com.rabbitmq.client.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
+import java.nio.charset.StandardCharsets
+
+@Component
+class Consumer {
+
+
+    val logger: Logger = LoggerFactory.getLogger(Consumer::class.java);
+
+
+    @Autowired
+    lateinit var gerenteService: GerenteService
+
+    @Bean
+     fun main() {
+        val factory = ConnectionFactory()
+        val connection = factory.newConnection("amqp://guest:guest@localhost:5672/")
+        val channel = connection.createChannel()
+        val consumerTag = "GerenteConsumer"
+
+        channel.queueDeclare("GERENTE", false, false, false, null)
+
+        logger.info("[$consumerTag] Waiting for messages...")
+        val deliverCallback = DeliverCallback { consumerTag: String?, delivery: Delivery ->
+            val message = String(delivery.body, StandardCharsets.UTF_8);
+            try {
+                val id = message.toLong();
+                logger.info("[$consumerTag] Received message: '$message'")
+                gerenteService.deleteService(id)
+            } catch (e: Error) {
+                logger.error("[$consumerTag] Failed to Consume Message: '$message'");
+            }
+
+        }
+        val cancelCallback = CancelCallback { consumerTag: String? ->
+            logger.error("[$consumerTag] was canceled")
+        }
+
+        channel.basicConsume("GERENTE", true, consumerTag, deliverCallback, cancelCallback)
+    }
+}
