57a1a313fbffa9e8fed4d38f63c477229758bd59
Gabriel Ganssin <gabriel.ganassin@contabilizei.com.br>
2022-09-11 15:31:05 -0300

    changed rabbit

--- src/main/kotlin/bantads/gerente/GerenteApplication.kt
+++ src/main/kotlin/bantads/gerente/GerenteApplication.kt
@@ -1,9 +1,7 @@
 package bantads.gerente
 
-import bantads.gerente.queue.Consumer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class GerenteApplication
